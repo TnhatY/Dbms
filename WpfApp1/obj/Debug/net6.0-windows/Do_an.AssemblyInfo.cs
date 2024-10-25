12e1aec400c6487c8103411481b11361493b9975
huanchaunhat <huanchaunhat@gmail.com>
2024-10-25 14:15:56 +0700

    Push lại

--- WpfApp1/obj/Debug/net6.0-windows/Do_an.AssemblyInfo.cs
+++ WpfApp1/obj/Debug/net6.0-windows/Do_an.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Do_an")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca5e502cf7d1735eba435ff66ae71ebb1bf45686")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3784182a29409bb57c50a8ab2e4dfed200460e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Do_an")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Do_an")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
