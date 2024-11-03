c7098117001f9b0fe25302037f0d7e22a3bd23a1
Hoang <134166368+koi132@users.noreply.github.com>
2024-11-03 23:44:34 +0700

    xóa nút xóa khách hàng, thêm ID để sửa khách hàng UC_KhachHang.xaml.cs

--- WpfApp1/UC_KhachHang.xaml.cs
+++ WpfApp1/UC_KhachHang.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Do_an.config;
+using Do_an.config;
 using Do_an.dao;
 using System;
 using System.Collections.Generic;
@@ -24,11 +24,13 @@ namespace Do_an
     /// </summary>
     public partial class UC_KhachHang : UserControl
     {
-        public UC_KhachHang()
+		private string selectedMAKH = "";
+
+		public UC_KhachHang()
         {
-            InitializeComponent();
-        }
-        public DataTable XemDanhMucKhachHang()
+            InitializeComponent();		
+		}
+        public DataTable XemDanhMucNhaCungCap()
         {
             SqlCommand cmd = new SqlCommand("SELECT * FROM DanhMucKhachHang", ConnectDB.getconnection());
             SqlDataAdapter adapter = new SqlDataAdapter(cmd);
@@ -39,28 +41,54 @@ namespace Do_an
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-            dg_khachhang.ItemsSource = XemDanhMucKhachHang().DefaultView;
+            dg_khachhang.ItemsSource = XemDanhMucNhaCungCap().DefaultView;
         }
+		private void RefreshCustomerDataGrid()
+		{
+			dg_khachhang.ItemsSource = XemDanhMucNhaCungCap().DefaultView;
+		}
 
-        private void btnTimkiem_Click(object sender, RoutedEventArgs e)
+		private void btnTimkiem_Click(object sender, RoutedEventArgs e)
         {
-            KhachHang_DAO khachHang_DAO = new KhachHang_DAO();
-            dg_khachhang.ItemsSource = khachHang_DAO.TimKiemKhachHang(txtsdt.Text).DefaultView;
-        }
+			if (string.IsNullOrWhiteSpace(txtsdt.Text))
+			{				
+				dg_khachhang.ItemsSource = XemDanhMucNhaCungCap().DefaultView;
+			}
+			else
+			{				
+				KhachHang_DAO khachHang_DAO = new KhachHang_DAO();
+				dg_khachhang.ItemsSource = khachHang_DAO.KhachHang_SDT(txtsdt.Text).DefaultView;
+			}
+		}
+		string ID;
         private void dg_khachhang_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (dg_khachhang.SelectedItem != null)
             {
                 var firstColumn = dg_khachhang.SelectedCells[0];
                 var firstColumnValue = ((TextBlock)firstColumn.Column.GetCellContent(firstColumn.Item)).Text;
-                makh.Text= firstColumnValue;
-            }
+                makh.Text = firstColumnValue;
+
+				var selectedRow = dg_khachhang.SelectedCells[0];
+				selectedMAKH = ((TextBlock)selectedRow.Column.GetCellContent(selectedRow.Item)).Text;
+
+				var khachhang = dg_khachhang.SelectedItem;
+				DataRowView rowView = (DataRowView)dg_khachhang.SelectedItem;
+				string maKH = rowView["MaKH"].ToString();
+				ID = maKH;
+			}
         }
 
         private void btnThemkhachhang_Click(object sender, RoutedEventArgs e)
         {
-            UC_ThemKhachHang uC_ThemKhachHang  = new UC_ThemKhachHang();
+			UC_ThemKhachHang uC_ThemKhachHang = new UC_ThemKhachHang();
             uC_ThemKhachHang.ShowDialog();
         }
-    }
+
+		private void suaKH_click(object sender, RoutedEventArgs e)
+		{
+			UC_SuaKhachHang uC_SuaKhachHang = new UC_SuaKhachHang(ID);
+			uC_SuaKhachHang.ShowDialog();
+		}	
+	}
 }
