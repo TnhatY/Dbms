e93d3d3bdd8e866e24573c584f840db232b8196a
Hoang <134166368+koi132@users.noreply.github.com>
2024-11-03 23:57:13 +0700

    thêm file cs usercontroll sửa khách hàng

---
+++ WpfApp1/obj/UC_SuaKhachHang.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Do_an.dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Do_an
+{
+    public partial class UC_SuaKhachHang : Window
+    {
+		public string maKH { get; set; }
+		public UC_SuaKhachHang(string ID)
+        {
+            InitializeComponent();
+			maKH = ID;
+        }        
+
+		private void btnSua_Click(object sender, RoutedEventArgs e)
+		{
+			KhachHang_DAO khachHang_DAO = new KhachHang_DAO();
+			khachHang_DAO.SuaKhachHang(maKH, sdt.Text, ten.Text, diachi.Text);
+		}
+	}
+}
